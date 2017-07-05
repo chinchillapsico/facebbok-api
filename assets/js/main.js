f776ec0be03000693a977e377b3fa93038bbb92a
chinchillapsico <coraline.colous@gmail.com>
2017-07-05 12:12:21 -0400

    eliminando boton de más

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,17 +26,6 @@ FB.getLoginStatus(function(response) {
 
 
 
-{
-    status: 'connected',
-    authResponse: {
-        accessToken: '...',
-        expiresIn:'...',
-        signedRequest:'...',
-        userID:'...'
-    }
-}
-
-
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
